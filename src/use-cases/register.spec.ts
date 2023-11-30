1586b694dcb93e73f4fe85636fd1f7a9947d88c2
Neto Rossetti <netinho.rossetti@gmail.com>
2023-11-30 09:31:36 -0300

    test: Testes unitários do UseCaseRegister.
    Neste ponto, foi criado um repositorio em memoria do usuário para que
    pudessemos realizar testes unitário das funcionalidades do resgistro de
    usuário sem a necessidade de integração com o banco de dados.

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,23 +1,26 @@
 import { expect, test, describe } from "vitest";
 import { RegisterUseCase } from "./register";
 import { compare } from "bcryptjs";
+import { InMemoryUsersRepository } from "src/repositories/in-memory/in-memory-users-repository";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 describe("Register Use Case", () => {
-  test("A senha do usuário precisa estar criptografada", async () => {
-    const registerUseCase = new RegisterUseCase({
-      async findByEmail(email) {
-        return null;
-      },
-      async create(data) {
-        return {
-          id: "id-1",
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        };
-      },
+  test("Deve ser possível se cadastrar", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: "Usuário 2",
+      email: "email.do.usuario@email.com",
+      password: "123",
     });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  test("A senha do usuário precisa estar criptografada", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
 
     const { user } = await registerUseCase.execute({
       name: "Nome do Usuário",
@@ -30,5 +33,26 @@ describe("Register Use Case", () => {
       user.password_hash
     );
     expect(isPasswordCorreclyHashed);
+  });
+
+  test("O usuário não deve poder se cadastrar com um e-mail duplicado", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
+
+    const email = "email.do.usuario@email.com";
+
+    await registerUseCase.execute({
+      name: "Usuário 1",
+      email,
+      password: "123456",
+    });
+
+    expect(async () => {
+      await registerUseCase.execute({
+        name: "Usuário 2",
+        email,
+        password: "123456",
+      });
+    }).rejects.toBeInstanceOf(UserAlreadyExistsError);
   });
 });
