3aaeea498ac64290fe63c2cb0b38f542d2b0b163
Neto Rossetti <netinho.rossetti@gmail.com>
2023-12-02 19:15:35 -0300

    feat: Configurando environment de teste e2e
    Neste commit foi feito uma configuração cabulosa para que o vitest
    podusse enchergar variaveis de ambiente para os teste e2e.
    
    Recomendo assistir essa abordagem no ignite caso queira implemeta-la!

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -9,8 +9,6 @@ export async function profile(request: FastifyRequest, reply: FastifyReply) {
     userId: request.user.sub,
   });
 
-  console.log(user);
-
   return reply.status(200).send({
     user: {
       ...user,
